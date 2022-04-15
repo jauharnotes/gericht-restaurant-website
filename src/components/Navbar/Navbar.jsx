3e357dda222c9a6a2bf2178ac8ab4d856be350b0
jauharnotes <jauharnotes@gmail.com>
2022-04-15 22:23:14 +0700

    Create navigations chef, intro and laurels

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,44 +2,43 @@ import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md'
 
-import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => { 
   const [togleMenu, setTogleMenu] = useState(false)
   return(
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <img src={images.gericht} alt='app logo' />
-      </div>
-      <ul className='app__navbar-links'>
-        <li className='p__opensans'><a href="#home">Home</a></li>
-        <li className='p__opensans'><a href="#pages">Pages</a></li>
-        <li className='p__opensans'><a href="#contact">Contact us</a></li>
-        <li className='p__opensans'><a href="#blog">Blog</a></li>
-        <li className='p__opensans'><a href="#landing">Landing</a></li>
-      </ul>
-      <div className='app__navbar-login'>
-        <a href="#login" className='p__opensans'>Log In / Register</a>
-        <span />
-        <a href="/" className='p__opensans'>Book Table</a>
-      </div>
-      <div className='app__navbar-smallscreen'>
-          <GiHamburgerMenu className='humberger-menu' color='#fff' fontSize={27} onClick={() => {setTogleMenu(true)}} />
-          {togleMenu && (     
-        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-          <MdOutlineRestaurantMenu color='#fff' fontSize={27} className='overlay__close' onClick={() => {setTogleMenu(false)}} />
-          <ul className='app__navbar-smallscreen-links'>
+      <nav className='app__navbar'>
+          <div className='app__navbar-logo'>
+            <p className='app__logo'>Gerícht</p>
+          </div>
+          <ul className='app__navbar-links'>
             <li className='p__opensans'><a href="#home">Home</a></li>
             <li className='p__opensans'><a href="#pages">Pages</a></li>
             <li className='p__opensans'><a href="#contact">Contact us</a></li>
             <li className='p__opensans'><a href="#blog">Blog</a></li>
             <li className='p__opensans'><a href="#landing">Landing</a></li>
-        </ul>
+          </ul>
+          <div className='app__navbar-login'>
+            <a href="#login" className='p__opensans'>Log In / Register</a>
+            <span />
+            <a href="/" className='p__opensans'>Book Table</a>
+          </div>
+        <div className='app__navbar-smallscreen'>
+            <GiHamburgerMenu className='humberger-menu' color='#fff' fontSize={27} onClick={() => {setTogleMenu(true)}} />
+            {togleMenu && (     
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu color='#fff' fontSize={27} className='overlay__close' onClick={() => {setTogleMenu(false)}} />
+            <ul className='app__navbar-smallscreen-links'>
+              <li className='p__opensans'><a href="#home">Home</a></li>
+              <li className='p__opensans'><a href="#pages">Pages</a></li>
+              <li className='p__opensans'><a href="#contact">Contact us</a></li>
+              <li className='p__opensans'><a href="#blog">Blog</a></li>
+              <li className='p__opensans'><a href="#landing">Landing</a></li>
+          </ul>
+          </div>
+            )}
         </div>
-          )}
-      </div>
-    </nav>
+      </nav>
 )};
 
 export default Navbar;
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import video from '../../assets/video-thumbnail.mp4';
 
 import './Intro.css';
 
 const Intro = () => (
-  <div>
-    Intro
+  <div className='i__video'>
+    <video width='100%' autoPlay="true" controls="controls">
+      <source src={video} type='video/mp4' />
+      Your browser not support the video tag.
+    </video>
   </div>
 );
 
